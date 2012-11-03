2d6fd734515a761078fbb853b7e5e58e0ba3eb25
Eddie Nolan <tapd260@gmail.com>
2012-11-02 23:31:44 -0400

    library stuff

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express'),
     app = express();
     app.use(express.bodyParser());
     app.use(app.router);
+    
+eval(fs.readFileSync('./graph.js')+'');
 
 if (process.argv.length <= 2) tcpport = 8888;
 else tcpport = process.argv[2]; //port of the HTTP server
@@ -44,72 +46,8 @@ function addTransaction(req, res) {
     var amount = req.body.amount;
     var description = req.body.description;
 
-    addTransactionHalf(borrower, lender, amount, description);
-    addTransactionHalf(lender, borrower, -amount, description);
+    addTransactionHalf(dg, borrower, lender, amount, description);
+    addTransactionHalf(dg, lender, borrower, -amount, description);
 
     res.send({status:1});
-}
-
-function addTransactionHalf(from, to, amount, description) {
-    if (dg[from] == undefined) {
-        dg[from] = {};
-        dg[from]["net"] = amount
-        dg[from]["neighbors"] = {};
-        dg[from]["neighbors"][to] = {};
-        dg[from]["neighbors"][to]["net"] = amount;
-        dg[from]["neighbors"][to]["transactions"] = [];
-        dg[from]["neighbors"][to]["transactions"][0] = {
-            "amount":amount,
-            "description":description
-        };
-    } else if (dg[from]["neighbors"][to] == undefined) {
-        dg[from]["net"] += amount;
-        dg[from]["neighbors"][to] = {};
-        dg[from]["neighbors"][to]["net"] = amount;
-        dg[from]["neighbors"][to]["transactions"] = [];
-        dg[from]["neighbors"][to]["transactions"][0] = {
-            "amount":amount,
-            "description":description
-        };
-    } else {
-        dg[from]["net"] += amount;
-        dg[from]["neighbors"][to]["net"] += amount;
-        var length = dg[from]["neighbors"][to]["transactions"].length
-        dg[from]["neighbors"][to]["transactions"][length] = {
-            "amount":amount,
-            "description":description
-        };
-    }
-}
-
-function getNet(name) {
-    if (dg[name] == undefined) {
-        return 0;
-    } else {
-        return dg[name]["net"];
-    }
-}
-
-function getNetBetween(from, to) {
-    if (dg[from] == undefined || dg[from]["neighbors"][to] == undefined) {
-        return 0;
-    } else {
-        return dg[from]["neighbors"][to]["net"];
-    }
-}
-
-function getNeighbors(name) {
-    if (dg[name] == undefined) {
-        return {};
-    } else {
-        return dg[name]["neighbors"];
-    }
-}
-
-function getTransactions(from, to) {
-    if (dg[from] == undefined || dg[from]["neighbors"][to] == undefined) {
-        return [];
-    } else {
-        return dg[from]["neighbors"][to]["transactions"];
-    }
 }
--- graph.js
+++ graph.js
@@ -1,10 +1,76 @@
-function findPaths(left, right, maxSize) {
-    val visited = {};
-    val paths = [];
+function addTransactionHalf(graph, from, to, amount, description) {
+    if (graph[from] == undefined) {
+        graph[from] = {};
+        graph[from]["net"] = amount
+        graph[from]["neighbors"] = {};
+        graph[from]["neighbors"][to] = {};
+        graph[from]["neighbors"][to]["net"] = amount;
+        graph[from]["neighbors"][to]["transactions"] = [];
+        graph[from]["neighbors"][to]["transactions"][0] = {
+            "amount":amount,
+            "description":description
+        };
+    } else if (graph[from]["neighbors"][to] == undefined) {
+        graph[from]["net"] += amount;
+        graph[from]["neighbors"][to] = {};
+        graph[from]["neighbors"][to]["net"] = amount;
+        graph[from]["neighbors"][to]["transactions"] = [];
+        graph[from]["neighbors"][to]["transactions"][0] = {
+            "amount":amount,
+            "description":description
+        };
+    } else {
+        graph[from]["net"] += amount;
+        graph[from]["neighbors"][to]["net"] += amount;
+        var length = graph[from]["neighbors"][to]["transactions"].length
+        graph[from]["neighbors"][to]["transactions"][length] = {
+            "amount":amount,
+            "description":description
+        };
+    }
+}
+
+function getNet(graph, name) {
+    if (graph[name] == undefined) {
+        return 0;
+    } else {
+        return graph[name]["net"];
+    }
+}
+
+function getNetBetween(graph, from, to) {
+    if (graph[from] == undefined || graph[from]["neighbors"][to] == undefined) {
+        return 0;
+    } else {
+        return graph[from]["neighbors"][to]["net"];
+    }
+}
+
+function getNeighbors(graph, name) {
+    if (graph[name] == undefined) {
+        return {};
+    } else {
+        return graph[name]["neighbors"];
+    }
+}
+
+function getTransactions(graph, from, to) {
+    if (graph[from] == undefined || graph[from]["neighbors"][to] == undefined) {
+        return [];
+    } else {
+        return graph[from]["neighbors"][to]["transactions"];
+    }
+}
+
+function findPaths(graph, left, right, maxSize) {
+    var visited = {};
+    var paths = [];
+
+    var frontier = newNeighbors(left); // maybe make next a priority queue
 
     function newNeighbors(name) {
-        val neighbors = getNeighbors(left);
-        val result = []
+        var neighbors = getNeighbors(name);
+        var result = []
         for (var neigbor in neighbors) {
             if (neighbor in visited) {
                 result[result.length] = neighbor;
@@ -12,15 +78,13 @@ function findPaths(left, right, maxSize) {
         }
         return result;
     }
-
-    val frontier = newNeighbors(left); // maybe make next a priority queue
-
+    
     while (frontier.length > 0) {
         // remove first element
-        val current = frontier.shift()
+        var current = frontier.shift()
         if (current = right) {
 	    current_path
-	}
+    }
 	visited[current] = true;
 	next.append(newNeighbors)
     }
