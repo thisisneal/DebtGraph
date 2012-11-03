820f03e9a0c605be86f66586dffbea36c54745ac
Neal Bhasin <neal@cmu.edu>
2012-11-02 21:07:25 -0400

    updated index to have a form, app reads the post request

--- app.js
+++ app.js
@@ -4,33 +4,37 @@ var express = require('express'),
     app.use(express.bodyParser());
     app.use(app.router);
 
-if (process.argv.length <= 3) tcpport = 8888;
-else tcpport = process.argv[3]; //port of the HTTP server
-    
+if (process.argv.length <= 2) tcpport = 8888;
+else tcpport = process.argv[2]; //port of the HTTP server
+
 app.get('/', function(req, res){
     indexer = fs.readFileSync('index.html').toString()
     res.send(indexer);
 });
 
-//Accept data (all pages, throws to doWork())
-app.post('/do_work_son', doWork, function(req, res,next){
+//Accept post request
+app.post('/addTransPost', addTrans, function(req, res) {
     //console.log(req.body)
-    res.send(req.body);
+    //res.send(req.body);
 });
 
 try {
     app.listen(tcpport);
-    console.log('Express server started on port ' + tcpport.toString() + 
-                '\nGo to http://localhost:'+ tcpport.toString() + '/ with your web browser.');
-}
-catch(err) {
+} catch(err) {
     console.log(err);
     console.log("Could not start express server!");
     process.exit();
 }
+console.log('Express server started on port ' + tcpport.toString() + 
+                '\nGo to http://localhost:'+ tcpport.toString() + 
+                '/ with your web browser.');
 
-function doWork(req, res)
-{
-    console.log(req);
-    res.send("not implemented");
+function addTrans(req, res) {
+    var lender = req.body.lender;
+    var borrower = req.body.borrower;
+    var amount = req.body.amount;
+
+    
+
+    res.send(req.body);
 }
