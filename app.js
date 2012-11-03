23d0c0efc5fa15c56227b55bc600d7649c82ae76
Eddie Nolan <tapd260@gmail.com>
2012-11-02 20:35:21 -0400

    added nonworking initial code

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
-var http = require('http');
+var fs = require('fs');
+var express = require('express'),
+    app = express.createServer();
+	app.use(express.bodyParser());
+	app.use(app.router);
 
-var db = {}
+if (process.argv.length <= 3) tcpport = 8888;
+else tcpport = process.argv[3]; //port of the HTTP server
+	
+app.get('/', function(req, res){
+	indexer = fs.readFileSync('index.html').toString()
+    res.send(indexer);
+});
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
+//Accept data (all pages, throws to doWork())
+app.post('/do_work_son', doWork, function(req, res,next){
+	//console.log(req.body)
+	res.send(req.body);
+});
 
-console.log('Server running at http://127.0.0.1:1337/');
+try {
+	app.listen(tcpport);
+	console.log('Express server started on port ' + app.address().port.toString() + 
+				'\nGo to http://localhost:'+app.address().port.toString() + '/ with your web browser.');
+}
+catch(err) {
+	console.log("Could not start express server!");
+	console.log("This is probably due to an existing node.js process that hasn't exited.");
+	console.log("Try running 'killall node'.");
+	process.exit();
+}
+
+function doWork(req, res)
+{
+	console.log(req);
+	res.send("not implemented");
+}
